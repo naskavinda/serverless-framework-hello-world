03c78c92a0a356c2cf7732397adbfbdfbf85df7e
naskavinda <naskavinda@gmail.com>
2023-03-29 09:52:33 +0530

    add POST endpoint

--- src/helloworld/handler.js
+++ src/helloworld/handler.js
@@ -8,3 +8,13 @@ module.exports.hello = async () => {
         }),
     };
 };
+
+module.exports.greet = async (event) => {
+  const name = JSON.parse(event.body).name;
+  const message = `Hello ${name}`;
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message })
+  };
+};
